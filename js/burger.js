74a32b72b8e52e5bcfc0444e0ebf294cb3a14fbd
tmdevgroup <tmdevgroup@users.noreply.github.com>
2021-01-04 13:32:39 +0500

    burger
    burger commit

---
+++ js/burger.js
@@ -0,0 +1,23 @@
+const navSlide = () => {
+   const burger = document.querySelector('.burger');
+   const nav = document.querySelector('.navbar_home');
+   const navLinks = document.querySelectorAll('.navbar_home-menu li')
+
+burger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+
+      navLinks.forEach((link, index) => {
+         if (link.style.animation) {
+            link.style.animation = '';
+         } else {
+            link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s';
+         }
+      });
+
+      burger.classList.toggle('toggle');
+   });
+
+   
+}
+
+navSlide();
